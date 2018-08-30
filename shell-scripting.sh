0b5c253869281f5e5b0ac1ccc56417a52cb9125c
HugoGustavo <hugog.valin@gmail.com>
2018-08-30 17:32:25 -0300

    Special Variables

---
+++ shell-scripting.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Autor: Hugo Gustavo
+
+echo "What is your name? "
+read PERSON
+echo "Hello, $PERSON"
---
+++ using-variables.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+NAME="Zara Ali"
+VAR1="Zara Ali"
+VAR2=100
+
+echo $NAME
+
+#readonly NAME
+unset NAME
+echo $NAME
