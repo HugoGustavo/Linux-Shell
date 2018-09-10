a7c088d57a266d0822347d7322d48b37a93ec592
HugoGustavo <hugog.valin@gmail.com>
2018-09-10 10:40:52 -0300

    quoting-mechanisms.sh

---
+++ quoting-mechanisms.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo Hello\;Word
+echo "I have \$1200"
+
+echo '<-$1500.**>;(update?) [y|n]'
+
+echo "It's Shell Programming"
+
+VAR=ZARA
+echo "$VAR owes <-\$1500.**>; [ as of (`date +%m/%d`) ]"
+
+DATE=`date`
+echo "Current date: $DATE"
